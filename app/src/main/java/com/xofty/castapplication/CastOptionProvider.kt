d4ced0e9dcc11445bedc846ed4dbe12e7157e2e2
Artem <blekart04@mail.ru>
2025-02-27 17:30:49 +0300

    ready

---
+++ app/src/main/java/com/xofty/castapplication/CastOptionProvider.kt
@@ -0,0 +1,20 @@
+package com.xofty.castapplication
+
+
+import android.content.Context
+import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+
+class CastOptionProvider : OptionsProvider {
+    override fun getCastOptions(context: Context): CastOptions {
+        return CastOptions.Builder()
+            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(context: Context): List<SessionProvider>? {
+        return null
+    }
+}
